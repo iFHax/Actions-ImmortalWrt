4ed2b5a4683768f3a3a985f1b8f7b95292c122d1
iFHax <ifhax@proton.me>
2025-04-16 23:35:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,10 +5,3 @@ sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
 # Navigate to the xray-core package directory and modify the version
 sed -i 's/PKG_VERSION:=v1.4.0/PKG_VERSION:=v1.8.24/' feeds/packages/net/xray-core/Makefile
-
-# Change system title to DotyWrt
-sed -i 's/ImmortalWrt/DotyWrt/g' package/base-files/files/etc/banner
-sed -i 's/ImmortalWrt/DotyWrt/g' package/base-files/files/bin/config_generate
-
-# Change default WiFi SSID to DotyWrt
-sed -i 's/ssid=ImmortalWrt/ssid=DotyWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
