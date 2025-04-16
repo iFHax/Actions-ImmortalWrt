c7fbef250d13090b0b65ebb31f482de84d9cacd7
iFHax <ifhax@proton.me>
2025-04-16 23:24:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
@@ -19,13 +8,12 @@ sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 # Partition alignment
 sed -i 's/256/4096/g' target/linux/x86/image/Makefile
 
-# Change Xray version to 1.5
 # Navigate to the xray-core package directory and modify the version
 sed -i 's/PKG_VERSION:=v1.4.0/PKG_VERSION:=v1.8.24/' feeds/packages/net/xray-core/Makefile
-# Xray-core v1.8.24
-# You can also manually download and set a specific version for Xray here
-# In case you're fetching the release via GitHub or custom sources, you can add additional steps if required
-# Example of downloading the specific version if required
-# curl -L https://github.com/XTLS/Xray-core/releases/download/v1.5.0/xray-linux-amd64-v1.5.0.tar.gz -o /tmp/xray-v1.5.0.tar.gz
-# tar -xzf /tmp/xray-v1.5.0.tar.gz -C /tmp
 
+# Change system title to DotyWrt
+sed -i 's/ImmortalWrt/DotyWrt/g' package/base-files/files/etc/banner
+sed -i 's/ImmortalWrt/DotyWrt/g' package/base-files/files/bin/config_generate
+
+# Change default WiFi SSID to DotyWrt
+sed -i 's/ssid=ImmortalWrt/ssid=DotyWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
