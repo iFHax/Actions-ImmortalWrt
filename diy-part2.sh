3e9450b5707da9067bc957dfb3660efd5e4772f0
iFHax <ifhax@proton.me>
2025-04-16 22:08:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,21 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # Replace ash with bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
 # Partition alignment
 sed -i 's/256/4096/g' target/linux/x86/image/Makefile
+
+# Change Xray version to 1.5
+# Navigate to the xray-core package directory and modify the version
+sed -i 's/PKG_VERSION:=v1.4.0/PKG_VERSION:=v1.5.0/' feeds/packages/net/xray-core/Makefile
+
+# You can also manually download and set a specific version for Xray here
+# In case you're fetching the release via GitHub or custom sources, you can add additional steps if required
+# Example of downloading the specific version if required
+# curl -L https://github.com/XTLS/Xray-core/releases/download/v1.5.0/xray-linux-amd64-v1.5.0.tar.gz -o /tmp/xray-v1.5.0.tar.gz
+# tar -xzf /tmp/xray-v1.5.0.tar.gz -C /tmp
+
